00241e860fed17272a9dd3fbbfa8533243d1e579
Xavier Schuszter <mankoty567@outlook.fr>
2024-12-13 09:51:33 +0100

    fix: add example through code to help beginners

--- api.tf
+++ api.tf
@@ -4,5 +4,7 @@ resource "docker_image" "api_image" {
 
 resource "docker_container" "api_container" {
   for_each = toset(["81", "82"])
+
+  name = "web-${index(["81", "82"], each.value) + 1}"
   # Add your instructions here
 }
